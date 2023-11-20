cdaf10814eb16776e0ea2538f54c7ebcef1c68fb
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2023-11-20 16:13:31 +0330

    Added plans

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
+import { randomUUID } from "crypto";
 
 import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
-import { CLIENTS } from "@/lib/constants";
+import CustomCard from "@/components/landing-page/custom-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { CardContent, CardDescription, CardTitle } from "@/components/ui/card";
+import { CLIENTS, PRICING_CARDS, PRICING_PLANS, USERS } from "@/lib/constants";
+import { cn } from "@/lib/utils";
+
 import Banner from "../../../public/appBanner.png";
 import CalBanner from "../../../public/cal.png";
+import Diamond from "../../../public/icons/diamond.svg";
+import CheckIcon from "../../../public/icons/check.svg";
 
 function HomePage() {
   return (
@@ -16,7 +24,7 @@ function HomePage() {
         />
         <div className="bg-white z-10 p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
           <Button
-            variant="secondary"
+            variant="btn-secondary"
             className="w-full rounded-[10px] p-6 text-2xl bg-background">
             Get MindMingle Free
           </Button>
@@ -88,6 +96,177 @@ function HomePage() {
             alt="Calendar Banner"
             className="rounded-2xl"
           />
+        </div>
+      </section>
+
+      <section className="relative">
+        <div className="w-full blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/50 -z-10 top-56" />
+        <div className="mt-20 px-4 sm:px-6 flex flex-col overflow-x-hidden overflow-visible">
+          <TitleSection
+            title="Trusted by all"
+            subheading="Join thousands of satisfied users who rely on our platform for their 
+          personal and professional productivity needs."
+            pill="Testimonials"
+          />
+          {[...Array(2)].map((_, index) => (
+            <div
+              key={randomUUID()}
+              className={cn(
+                "mt-10 flex flex-nowrap gap-6 self-start",
+                {
+                  "flex-row-reverse": index === 1,
+                  "animate-[slide_250s_linear_infinite]": true,
+                  "animate-[slide_250s_linear_infinite_reverse]": index === 1,
+                  "ml-[100vw]": index === 1,
+                },
+                "hover:paused"
+              )}>
+              {USERS.map((testimonial, index) => (
+                <CustomCard
+                  key={testimonial.name}
+                  className="w-[500px]
+                shrink-0s
+                rounded-xl
+                dark:bg-gradient-to-t
+                dark:from-border dark:to-background
+              "
+                  cardHeader={
+                    <div
+                      className="flex
+                    items-center
+                    gap-4
+                ">
+                      <Avatar>
+                        <AvatarImage src={`/avatars/${index + 1}.png`} />
+                        <AvatarFallback>AV</AvatarFallback>
+                      </Avatar>
+                      <div>
+                        <CardTitle className="text-foreground">
+                          {testimonial.name}
+                        </CardTitle>
+                        <CardDescription className="dark:text-washed-purple-800">
+                          {testimonial.name.toLocaleLowerCase()}
+                        </CardDescription>
+                      </div>
+                    </div>
+                  }
+                  cardContent={
+                    <p className="dark:text-washed-purple-800">
+                      {testimonial.message}
+                    </p>
+                  }></CustomCard>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <section
+        className="mt-20
+        px-4
+        sm:px-6
+      ">
+        <TitleSection
+          title="The Perfect Plan For You"
+          subheading="Experience all the benefits of our platform. Select a plan that suits your needs and take your productivity to new heights."
+          pill="Pricing"
+        />
+        <div
+          className="flex 
+        flex-col-reverse
+        sm:flex-row
+        gap-4
+        justify-center
+        sm:items-stretch
+        items-center
+        mt-10
+        ">
+          {PRICING_CARDS.map(card => (
+            <CustomCard
+              key={card.planType}
+              className={cn(
+                "w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative",
+                {
+                  "border-brand-primaryPurple/70":
+                    card.planType === PRICING_PLANS.proplan,
+                }
+              )}
+              cardHeader={
+                <CardTitle
+                  className="text-2xl
+                  font-semibold
+              ">
+                  {card.planType === PRICING_PLANS.proplan && (
+                    <>
+                      <div
+                        className="hidden dark:block w-full blur-[120px] rounded-full h-32
+                        absolute
+                        bg-brand-primaryPurple/80
+                        -z-10
+                        top-0
+                      "
+                      />
+                      <Image
+                        src={Diamond}
+                        alt="Pro Plan Icon"
+                        className="absolute top-6 right-6"
+                      />
+                    </>
+                  )}
+                  {card.planType}
+                </CardTitle>
+              }
+              cardContent={
+                <CardContent className="p-0">
+                  <span
+                    className="font-normal 
+                    text-2xl
+                ">
+                    ${card.price}
+                  </span>
+                  {+card.price > 0 ? (
+                    <span className="dark:text-washed-purple-800 ml-1">
+                      /mo
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                  <p className="dark:text-washed-purple-800">
+                    {card.description}
+                  </p>
+                  <Button
+                    variant="btn-primary"
+                    className="whitespace-nowrap w-full mt-4">
+                    {card.planType === PRICING_PLANS.proplan
+                      ? "Go Pro"
+                      : "Get Started"}
+                  </Button>
+                </CardContent>
+              }
+              cardFooter={
+                <ul
+                  className="font-normal
+                  flex
+                  mb-2
+                  flex-col
+                  gap-4
+                ">
+                  <small>{card.highlightFeature}</small>
+                  {card.freatures.map(feature => (
+                    <li
+                      key={feature}
+                      className="flex
+                      items-center
+                      gap-2
+                    ">
+                      <Image src={CheckIcon} alt="Check Icon" />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              }
+            />
+          ))}
         </div>
       </section>
     </>

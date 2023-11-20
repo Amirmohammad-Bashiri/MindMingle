95d97b052da9c1b51e4360f702dfe471aefd99cb
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2023-11-20 10:46:19 +0330

    Implemented brands showcase

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,5 +1,5 @@
-function HomePageLayout() {
-  return <div>homepage layout</div>;
+function HomePageLayout({ children }: { children: React.ReactNode }) {
+  return <main>{children}</main>;
 }
 
 export default HomePageLayout;

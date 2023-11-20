747470435394036faddd60375f6dd6ff9148d3d0
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2023-11-20 11:02:33 +0330

    Added Calendar section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
-import Banner from "../../..//public/appBanner.png";
 import { CLIENTS } from "@/lib/constants";
+import Banner from "../../../public/appBanner.png";
+import CalBanner from "../../../public/cal.png";
 
 function HomePage() {
   return (
@@ -71,6 +72,22 @@ function HomePage() {
               ))}
             </div>
           ))}
+        </div>
+      </section>
+
+      <section className="px-4 sm:px-6 flex justify-center items-center flex-col relative">
+        <div className="w-[30%] blur-[120px] rounded-full h-32 absolute bg-brand-primaryPurple/50 -z-10 top-22" />
+        <TitleSection
+          title="Keep track of your meetings all in one place"
+          subheading="Capture your ideas, thoughts, and meeting notes in a structured and organized manner."
+          pill="Features"
+        />
+        <div className="mt-10 max-w-[450px] flex justify-center items-center relative sm:ml-0 rounded-2xl border-8 border-washed-purple-300 border-opacity-10">
+          <Image
+            src={CalBanner}
+            alt="Calendar Banner"
+            className="rounded-2xl"
+          />
         </div>
       </section>
     </>
